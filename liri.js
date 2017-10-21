46c912196832c37d3e4d985a76f7a0a2ba5d6091
alexdecycive <alex@decycive.com>
2017-10-21 17:13:41 -0400

    Twitter and Spotify working, need to finish the other 2 functions

--- liri.js
+++ liri.js
@@ -20,31 +20,93 @@ var fs = require("fs");
 var Twitter = require("twitter");
 var request = require("request");
 var prompt = require("prompt");
+var Spotify = require('node-spotify-api');
 
-var tKeys = require('./Keys.js');
-console.log(tKeys);
+//var tKeys = require('./Keys.js');
+//console.log(tKeys);
 
 var client = new Twitter ({
-  tKeys
+  consumer_key: 'L73w7ZBdGn8QyZLAsNCBhKzxY',
+  consumer_secret: 'vU0QVQLlYy6TmRZOmRkE0obev9n77U53UTrGhIY35tdH20UL2G',
+  access_token_key: '918248754611916803-mifTGpdq6HiCcR47LNuOivSnfrPmiVY',
+  access_token_secret: 'QRx9h9OE2U0NvbPRxgdcHSbJRxJo79NYVwmPCReVj3e3C',
+});
+
+var spotifyvar = new Spotify({
+  id: '247841dc8deb4be4b442213c66dcfb77',
+  secret: '92d1577922c543248b03d2ec6ba6a84b'
 });
 
 
+
  prompt.get(['command'], function (err, result) {
-    // 
     // Log the results. 
     console.log(' --> You commanded me to: ' + result.command);
 
     if (result.command === "mytweets") {
-    	console.log("Okay, I will print your latest tweets...")
-		var params = {screen_name: 'alexsaaats'};
-		client.get('statuses/user_timeline', params, function(error, tweets, response) {
-	  		//if(error) throw error;
-  			console.log(tweets);  // The favorites. 
-  			//console.log(response);  // Raw response object. 
-		});
-	}
+
+      console.log("Okay, I will print @alexsaaats latest tweets...")
+  		var params = {screen_name: 'alexsaaats'};
+
+  		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  	  		//if(error) throw error;
+    			console.log(tweets);  // The favorites. 
+    			//console.log(response);  // Raw response object. 
+  		});
+    }
 
 
+          else if (result.command === "spotify-this-song") {
+            prompt.get(['song'], function (err, result) {
+
+              console.log("Okay, I will get info about " + result.song + " for you...")
+                
+              spotifyvar.search({ type: 'track', query: result.song }, function(err, data) {
+              if (err) {
+                return console.log('Error occurred: ' + err);
+              }
+              var artistname = data.tracks.items[0].album.artists[0].name;
+              var songname = data.tracks.items[0].name;
+              var previewlink = data.tracks.items[0].preview_url;
+              var albumname = data.tracks.items[0].album.name;
+              var songcount = data.tracks.total;
+
+              //console.log(data);
+              console.log("-------------------------------");
+              console.log("-------------------------------");
+              console.log("Artist: " + artistname);
+              console.log("Song Name: " + songname);
+              console.log("Preview Link: " + previewlink);
+              console.log("Album Name: " + albumname);
+              console.log("-------------------------------");
+              console.log("NOTE: This is only the first of many songs you might have been looking for.")
+              console.log("NOTE: Total count of songs with " + result.song + " in the name: " + songcount);
+              console.log("-------------------------------");
+              console.log("-------------------------------");
+
+              });
+            });
+          }
+
+
+                else if (result.command === "movie-this") {
+                  prompt.get(['movie'], function (err, result) {
+
+                    console.log("Okay, I will get info about " + result.movie + " for you...")
+                      
+                    spotifyvar.search({ type: 'track', query: result.song }, function(err, data) {
+                    if (err) {
+                      return console.log('Error occurred: ' + err);
+                    }
+                    console.log(data); 
+                    });
+                  });
+                }
+
+
+                    else (console.log("I don't understand what you're asking me to do. You can say mytweets, spotify-this-song, or movie-this. Please start over."));
+
+	  
 
   });
 
@@ -53,9 +115,9 @@ var client = new Twitter ({
 
 //Take these commands: 
    //* `my-tweets`
-var mytweets = process.argv[2];
-if (mytweets === "my-tweets") {
-	}
+//var mytweets = process.argv[2];
+//if (mytweets === "my-tweets") {
+	//}
 
    //* `spotify-this-song`
 var spotify = process.argv[2];
