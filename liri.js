3e0e201f8e4ae69ed7d3a3420b9c15f311f13c1e
alexdecycive <alex@decycive.com>
2017-10-22 00:07:07 -0400

    Added logging to log.txt

--- liri.js
+++ liri.js
@@ -5,12 +5,21 @@ console.log("|||   Starting boosters...  |||");
 console.log("|||   ***** READY *****     |||");
 console.log("-------------------------------");
 
+
 //Set packages
 var fs = require("fs");
 var Twitter = require("twitter");
 var request = require("request");
 var prompt = require("prompt");
 var Spotify = require('node-spotify-api');
+let date = require('date-and-time');
+let now = new Date();
+
+fs.appendFile('log.txt', "LIRI has been run at: " + date.format(now, 'YYYY/MM/DD HH:mm:ss')  + ",", function(err) {
+        if (err) {
+        console.log(err);
+        }
+      });
 
 
 //var tKeys = require('./Keys.js');
@@ -39,6 +48,11 @@ var omdbkey = '40e9cece';
  prompt.get(['command'], function (err, result) {
     // Log the results. 
     console.log(' --> You commanded me to: ' + result.command);
+      fs.appendFile('log.txt', "The command was: " + result.command + " logged at " + date.format(now, 'YYYY/MM/DD HH:mm:ss') + ",", function(err) {
+        if (err) {
+        console.log(err);
+        }
+      });
 
     if (result.command === "my-tweets") {
 
@@ -47,8 +61,21 @@ var omdbkey = '40e9cece';
 
   		client.get('statuses/user_timeline', params, function(error, tweets, response) {
   	  		//if(error) throw error;
-    			console.log(tweets);  
+    			//console.log(tweets);  
+          console.log("TWEET 1 --------------------");
+          console.log("Tweet content: " + tweets[0].text);
+          console.log("Tweet Created Time: " + tweets[0].created_at);
+          console.log("TWEET 2 --------------------");
+          console.log("Tweet content: " + tweets[1].text);
+          console.log("Tweet Created Time: " + tweets[1].created_at);
+
     			//console.log(response);  // Raw response object. 
+
+          fs.appendFile('log.txt', "The result of this was: " + tweets[0].text + " on " + tweets[0].created_at + " logged at " + date.format(now, 'YYYY/MM/DD HH:mm:ss') + ",", function(err) {
+          if (err) {
+          console.log(err);
+          }
+          });
   		});
     }
 
@@ -59,28 +86,34 @@ var omdbkey = '40e9cece';
               console.log("Okay, I will get info about " + result.song + " for you...")
                 
               spotifyvar.search({ type: 'track', query: result.song }, function(err, data) {
-              if (err) {
-                return console.log('Error occurred: ' + err);
-              }
-              var artistname = data.tracks.items[0].album.artists[0].name;
-              var songname = data.tracks.items[0].name;
-              var previewlink = data.tracks.items[0].preview_url;
-              var albumname = data.tracks.items[0].album.name;
-              var songcount = data.tracks.total;
+                if (err) {
+                  return console.log('Error occurred: ' + err);
+                }
 
-              //console.log(data);
-              console.log("-------------------------------");
-              console.log("-------------------------------");
-              console.log("Artist: " + artistname);
-              console.log("Song Name: " + songname);
-              console.log("Preview Link: " + previewlink);
-              console.log("Album Name: " + albumname);
-              console.log("-------------------------------");
-              console.log("NOTE: This is only the first of many songs you might have been looking for.")
-              console.log("NOTE: Total count of songs with " + result.song + " in the name: " + songcount);
-              console.log("-------------------------------");
-              console.log("-------------------------------");
+                var artistname = data.tracks.items[0].album.artists[0].name;
+                var songname = data.tracks.items[0].name;
+                var previewlink = data.tracks.items[0].preview_url;
+                var albumname = data.tracks.items[0].album.name;
+                var songcount = data.tracks.total;
 
+                //console.log(data);
+                console.log("-------------------------------");
+                console.log("-------------------------------");
+                console.log("Artist: " + artistname);
+                console.log("Song Name: " + songname);
+                console.log("Preview Link: " + previewlink);
+                console.log("Album Name: " + albumname);
+                console.log("-------------------------------");
+                console.log("NOTE: This is only the first of many songs you might have been looking for.")
+                console.log("NOTE: Total count of songs with " + result.song + " in the name: " + songcount);
+                console.log("-------------------------------");
+                console.log("-------------------------------");
+
+                fs.appendFile('log.txt', "The result of this was: " + songname + " by " + artistname + " logged at " + date.format(now, 'YYYY/MM/DD HH:mm:ss') + ",", function(err) {
+                  if (err) {
+                  console.log(err);
+                  }
+                });
               });
             });
           }
@@ -96,23 +129,29 @@ var omdbkey = '40e9cece';
                     request("http://www.omdbapi.com/?apikey=" + omdbkey + "&t=" + result.movie, function(error, response, data) {
                     // If the request was successful...
                       if (!error && response.statusCode === 200) {
-                      var moviedata = JSON.parse(data);
-                      
-                      //console.log(data);
-                      //console.log(moviedata);
+                        var moviedata = JSON.parse(data);
+                        
+                        //console.log(data);
+                        //console.log(moviedata);
 
-                      console.log("-------------------------------");
-                      console.log("-------------------------------");
-                      console.log("Title: " + moviedata.Title);
-                      console.log("Year: " + moviedata.Year);
-                      console.log("IMDB Rating: " + moviedata.Ratings[0].Value);
-                      console.log("Rotten Tomatoes Rating: " + moviedata.Ratings[1].Value);
-                      console.log("Country: " + moviedata.Country);
-                      console.log("Language: " + moviedata.Language);
-                      console.log("Plot: " + moviedata.Plot);
-                      console.log("Actors: " + moviedata.Actors);
-                      console.log("-------------------------------");
-                      console.log("-------------------------------");
+                        console.log("-------------------------------");
+                        console.log("-------------------------------");
+                        console.log("Title: " + moviedata.Title);
+                        console.log("Year: " + moviedata.Year);
+                        console.log("IMDB Rating: " + moviedata.Ratings[0].Value);
+                        console.log("Rotten Tomatoes Rating: " + moviedata.Ratings[1].Value);
+                        console.log("Country: " + moviedata.Country);
+                        console.log("Language: " + moviedata.Language);
+                        console.log("Plot: " + moviedata.Plot);
+                        console.log("Actors: " + moviedata.Actors);
+                        console.log("-------------------------------");
+                        console.log("-------------------------------");
+
+                        fs.appendFile('log.txt', "The result of this was: " + moviedata.Title + " starring " + moviedata.Actors + " logged at " + date.format(now, 'YYYY/MM/DD HH:mm:ss') + ",", function(err) {
+                          if (err) {
+                          console.log(err);
+                          }
+                        });
                       }
                     });
                   });
@@ -140,20 +179,30 @@ var omdbkey = '40e9cece';
                           var randomnumber = Math.round((Math.random() * 1) + 1);
                           console.log("Your random number is: " + randomnumber);
 
+                            fs.appendFile('log.txt', "The result of this was: RANDOMNUMBER = " + randomnumber + " - where 1 is GetSpotify and 2 is GetMovie -" + " logged at " + date.format(now, 'YYYY/MM/DD HH:mm:ss') + ",", function(err) {
+                              if (err) {
+                              console.log(err);
+                              }
+                            });
+
                           if (randomnumber === 1) {
                             getspotify(input1);
                           }
                             else if (randomnumber === 2) {
                               getmovie(input2);
                             }
-
                         });
-                        
                       }
+
+                       // 
 
                           //If they didn't ask for any known function, tell them you're confused and provide directions.
                           else (console.log("I don't understand what you're asking me to do. You can say mytweets, spotify-this-song, movie-this, or do-what-it-says. Please start over."));
-
+                            fs.appendFile('log.txt', "The user entered a command that was not recognized. Logged at " + date.format(now, 'YYYY/MM/DD HH:mm:ss') + ",", function(err) {
+                              if (err) {
+                              console.log(err);
+                              }
+                            });
 	  
 //Close the first 'command' prompt
   });
